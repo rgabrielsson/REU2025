cacb60c81b5213ae5be4b4edf457cce898a48d1c
Rebecca Gabrielsson <rmgabrielsson23@coe.edu>
2025-06-23 20:52:07 +0000

    Find lowest k written, seems to cause problems

--- Counting.py
+++ Counting.py
@@ -1,47 +1,70 @@
-combos = []
-for a in range(11):
-    for b in range(11):
-        for c in range(11):
-            if a + b + c == 9 or a + b + c == 10:
-                combos.append((a,b,c))
-            elif a == 9 or a == 10:
-                combos.append((a,b,c))
-            elif b == 9 or b == 10:
-                combos.append((a,b,c))
-            elif c == 9 or c == 10:
-                combos.append((a,b,c))
-            elif a + b == 9 or a + b == 10:
-                combos.append((a,b,c))
-            elif c + b == 9 or c + b == 10:
-                combos.append((a,b,c))
-            elif a + c == 9 or a + c == 10:
-                combos.append((a,b,c))
+import math
+
+def test_cases():
+    testset = [9,10]
+
+    combos = []
+    for a in range(11):
+        for b in range(11):
+            for c in range(11):
+                if a + b + c in testset:
+                    combos.append((a,b,c))
+                elif a in testset:
+                    combos.append((a,b,c))
+                elif b in testset:
+                    combos.append((a,b,c))
+                elif c in testset:
+                    combos.append((a,b,c))
+                elif a + b in testset:
+                    combos.append((a,b,c))
+                elif c + b in testset:
+                    combos.append((a,b,c))
+                elif a + c in testset:
+                    combos.append((a,b,c))
 
 
-maxcount = 0
-for combo in combos:
-    count = 0
-    a = combo[0]
-    b = combo[1]
-    c = combo[2]
+    maxcount = 0
+    for combo in combos:
+        count = 0
+        a = combo[0]
+        b = combo[1]
+        c = combo[2]
 
-    if a == 9 or a == 10:
-        count += 1
-    if b == 9 or b == 10:
-        count += 1
-    if c == 9 or c == 10:
-        count += 1
-    if a + b == 9 or a + b == 10:
-        count += 1
-    if c + b == 9 or c + b == 10:
-        count += 1
-    if a + c == 9 or a + c == 10:
-        count += 1
-    if a + b +c == 9 or a + b + c == 10:
-        count += 1
-    if count > maxcount:
-        maxcount = count
-    if count == 3:
-        print(combo)
+        if a in testset:
+            count += 1
+        if b in testset:
+            count += 1
+        if c in testset:
+            count += 1
+        if a + b in testset:
+            count += 1
+        if c + b in testset:
+            count += 1
+        if a + c in testset:
+            count += 1
+        if a + b +c in testset:
+            count += 1
+        if count > maxcount:
+            maxcount = count
+        if count == 3 and a != 1 and a != 1 and a != 1:
+            print(combo)
+            print(a+b+c)
 
-print(maxcount)
+    print(maxcount)
+
+
+def find_lowest_k(ving):
+    start_charge = 2**(ving-1) * (14-ving)
+    charge = 4096
+    k=12
+    while charge >= 0:
+        k=k+1
+        charge = ((math.factorial(k)/(math.factorial(ving)*math.factorial(k-ving)))*start_charge + 2**(k-1) * (14-k))/((math.factorial(k)/(math.factorial(ving)*math.factorial(k-ving)))+1)
+    print(ving)
+    print(k)
+    print(charge)
+    print("---------")
+
+
+for i in range(14):
+    find_lowest_k(i)
